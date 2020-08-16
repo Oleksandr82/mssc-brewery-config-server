1503c30943b69350cd376a0cf57448c2d51dd549
Oleksandr Sytnik <Oleksandr.Sytnik@rabobank.nl>
2020-08-16 19:30:27 +0200

    Initial commit for the configuration server

---
+++ src/test/java/tech/nautilus/brewery/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.nautilus.brewery.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
